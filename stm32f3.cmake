6021d50a6c75ec445192755c3df492e3c0b35514
Nicolas Veloz Savino <nicolas.veloz.savino@gmail.com>
2019-02-18 16:12:12 +0100

    Adding f3

---
+++ stm32f3.cmake
@@ -0,0 +1,56 @@
+
+
+set(STM32_FAMILY "f3")
+
+
+
+set(CFLAGS_1
+        -mcpu=cortex-m4
+        -mthumb
+        -mfloat-abi=hard
+        -std=c99
+        -D__weak=__attribute__\(\(weak\)\)
+        -D__packed=__attribute__\(\(__packed__\)\)
+        )
+
+set(CFLAGS_2
+        -Os
+        -g3
+        -pedantic
+        -Wall
+        -Werror
+        -fmessage-length=0
+        -ffunction-sections
+        -c
+        -fmessage-length=0
+        )
+
+
+set(ASMFLAGS
+        -mcpu=cortex-m4
+        -mthumb
+        -mfloat-abi=hard
+        -g
+        )
+
+set(CXXFLAGS "")
+
+set(LDFLAGS_1
+        -mcpu=cortex-m4
+        -mthumb
+        -mfloat-abi=hard
+        -specs=nosys.specs
+        #        -specs=nano.specs
+        -mfpu=fpv4-sp-d16
+        )
+
+
+set(LDFLAGS_2
+        -Wl,-Map=output.map -Wl,--gc-sections
+        )
+
+
+set(OPENOCD_CFG "board/st_nucleo_f3.cfg")
+
+include(arm-toolchain)
+include(stm32)
